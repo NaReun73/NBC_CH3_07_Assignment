4ecca67ceb05d2dedc8b4cc9d68da378394180d6
NaReun73 <rnlcksg37@gmail.com>
2026-04-13 20:48:07 +0900

    MyPlayerController
    키 맵핑 완료
    IMC 활성화

--- Source/NBC_CH3_07_Project/Private/MyGameMode.cpp
+++ Source/NBC_CH3_07_Project/Private/MyGameMode.cpp
@@ -1,7 +1,9 @@
 #include "MyGameMode.h"
 #include "PlayerPawn.h"
+#include "MyPlayerController.h"
 
 AMyGameMode::AMyGameMode()
 {
 	DefaultPawnClass = APlayerPawn::StaticClass();
+	PlayerControllerClass = AMyPlayerController::StaticClass();
 }
---
+++ Source/NBC_CH3_07_Project/Private/MyPlayerController.cpp
@@ -0,0 +1,30 @@
+#include "MyPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AMyPlayerController::AMyPlayerController()
+	: InputMappingContext(nullptr),
+	  MoveAction(nullptr),
+	  JumpAction(nullptr),
+	  LookAction(nullptr)
+{
+
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 현재 PlayerController에 연결된 Local Player 객체를 가져옴
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		// Local Player에서 EnhancedInputLocalPlayerSubsystem을 획득
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext)
+			{
+				// 설정한 IMC 활성화 (우선순위 0)
+				Subsystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
